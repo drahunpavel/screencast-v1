3b0e7c97bef36a4506149798c2369b34a33817ed
drahunpavel <drahunpavel@gmail.com>
2018-06-26 02:46:16 +0300

    завершенно Отображение массивов, смысл аттрибута key

--- app/src/component/ArticleList.js
+++ app/src/component/ArticleList.js
@@ -1,13 +1,20 @@
-import React, {Component} from 'react'
+import React from 'react'
 import Article from './Article'
 
-export default function ArticleList({articles}) {
+export default function ArticleList({articles}) {//в пропсах приходит массив статей articles
+    
+    //превращаем каждый объект articles в реактовский элемент
+    const articleElements = articles.map(article =>
+        <li key={article.id}><Article article={article}/></li>
+    )
+    
     return(
+
         <ul>
-            <li>Article article={articles[0]}</li>
-            <li>Article article={articles[1]}</li>
-            <li>Article article={articles[2]}</li>
-            <li>Article article={articles[3]}</li>
+            {/* <li><Article article={articles[0]}/></li>
+            <li><Article article={articles[1]}/></li>
+            <li><Article article={articles[2]}/></li> */}
+            {articleElements}
         </ul>
     )
 }
